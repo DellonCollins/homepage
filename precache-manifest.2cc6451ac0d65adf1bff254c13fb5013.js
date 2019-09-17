70f87b66844f6048372103f3d541846e7eb30f34
Dellon Collins <dellon.collins@ad.infosys.com>
2019-09-17 12:03:23 -0400

    Updates

--- precache-manifest.2cc6451ac0d65adf1bff254c13fb5013.js
+++ precache-manifest.2cc6451ac0d65adf1bff254c13fb5013.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "138fc7d531e05974c81691cf97987aea",
+    "revision": "6e6f1160359b6d531682690da9573d7d",
     "url": "/homepage/index.html"
   },
   {
-    "revision": "c2f39498c8586bfc0291",
-    "url": "/homepage/static/css/main.4066e6c4.chunk.css"
+    "revision": "113353b9627c2c7e7293",
+    "url": "/homepage/static/css/main.e4ec4bae.chunk.css"
   },
   {
     "revision": "6ffab5db66587db9167a",
     "url": "/homepage/static/js/2.7d663209.chunk.js"
   },
   {
-    "revision": "c2f39498c8586bfc0291",
+    "revision": "113353b9627c2c7e7293",
     "url": "/homepage/static/js/main.03136be0.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/homepage/precache-manifest.3bf00e11661757c97da689779d00f429.js"
+  "/homepage/precache-manifest.2cc6451ac0d65adf1bff254c13fb5013.js"
 );
 
 self.addEventListener('message', (event) => {
